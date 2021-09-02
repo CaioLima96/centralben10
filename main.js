8aebea10579c9f8298746b3fac591afec144ce01
CaioLima96 <62268811+CaioLima96@users.noreply.github.com>
2021-09-02 18:03:34 -0300

    começo

---
+++ main.js
@@ -0,0 +1,78 @@
+function alienForceTab () {
+
+    let alienForceTab = document.getElementById('nav-tab-content').innerHTML = `
+    <div id="sinopse">
+
+                    <h1>SINOPSE</h1>
+                    <p> 
+                        Cinco anos após a série original, Ben reaparece sem o Omnitrix , até o desaparecimento de seu avô Max, tendo que colocá-lo de volta, reiniciando e dando a Ben um novo conjunto de 10 alienígenas. Junto de Gwen e seu antigo inimigo Kevin eles partem em missões encarando novos vilões e adquirindo novos alienígenas pelo tempo. A série fora originalmente intitulado Ben 10: Hero Generation (em tradução livre: Ben 10: Geração de Heróis).
+                    </p>
+
+                </div>
+
+                <!--PRIME BEN'S ALIEN CARDS-->
+                <ul id="aliens-cards">
+                
+                    <li id="alien-card" class="ripJaws">
+                        
+                        <div id="alien-card-img">
+                            <img src="./aquatico-(classico)-7.png" alt="Retângulo 1" class="img-fluid" data-toggle="modal" data-target="#ClassicBenFourArmsModal">
+                        </div>
+
+                        <div id="alien-card-description">
+                            <a href="/" target="_blank">
+                                <p>Aquatico</p>
+                            </a>
+                        </div>
+                         
+                    </li>
+
+                    <li id="alien-card" class="ripJaws">
+                        
+                        <div id="alien-card-img">
+                            <img src="./aquatico-(classico)-7.png" alt="Retângulo 1" class="img-fluid" data-toggle="modal" data-target="#ClassicBenFourArmsModal">
+                        </div>
+
+                        <div id="alien-card-description">
+                            <a href="/" target="_blank">
+                                <p>Aquatico</p>
+                            </a>
+                        </div>
+                         
+                    </li>
+
+                    <li id="alien-card" class="ripJaws">
+                        
+                        <div id="alien-card-img">
+                            <img src="./aquatico-(classico)-7.png" alt="Retângulo 1" class="img-fluid" data-toggle="modal" data-target="#ClassicBenFourArmsModal">
+                        </div>
+
+                        <div id="alien-card-description">
+                            <a href="/" target="_blank">
+                                <p>Aquatico</p>
+                            </a>
+                        </div>
+                         
+                    </li>
+
+                    <li id="alien-card" class="ripJaws">
+                        
+                        <div id="alien-card-img">
+                            <img src="./aquatico-(classico)-7.png" alt="Retângulo 1" class="img-fluid" data-toggle="modal" data-target="#ClassicBenFourArmsModal">
+                        </div>
+
+                        <div id="alien-card-description">
+                            <a href="/" target="_blank">
+                                <p>Aquatico</p>
+                            </a>
+                        </div>
+                         
+                    </li>
+
+                </ul>
+    
+    
+    `
+
+    return alienForceTab
+}
