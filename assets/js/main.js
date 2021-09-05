14933c023adc82032a19019f19c5f9d900359343
CaioLima96 <62268811+CaioLima96@users.noreply.github.com>
2021-09-05 20:14:50 -0300

    versão inicial da home feita

--- assets/js/main.js
+++ assets/js/main.js
@@ -84,3 +84,271 @@ function alienForceTab () {
 
     return alienForceTab
 }
+
+function ultimateAlienTab() {
+    
+    let ultimateAlienTab = document.getElementById('nav-tab-content').innerHTML = `
+    <div id="sinopse">
+
+        <h1>SINOPSE</h1>
+        <p> 
+            Algumas semanas após Ben 10: Força Alienígena, Ben possui um novo dispositivo: o Super Omnitrix (que permite virar <a href="#ultimate-aliens-ultimates">versões supremas</a> de seus aliens), além de ter que lidar com o fato da sua identidade secreta ter sido revelada. Os antigos vilões prometem retornar e novas ameaças ao universo surgem, mas Gwen e Kevin também voltam e estão prontos para dar o apoio que o herói precisa.
+        </p>
+
+    </div>
+
+    <div id="indice-list">
+
+        <p>Índice</p>
+
+        <li><a href="#ultimate-aliens-ultimates">Aliens Supremos</a></li>
+        <li><a href="#classic-aliens-ua">Aliens do Clássico</a></li>
+
+    </div>
+
+    <!--PRIME BEN'S ALIEN CARDS-->
+    <ul id="aliens-cards">
+
+        <li id="alien-card" class="ripJaws">
+            
+            <div id="alien-card-img">
+                <img src="./macaco-aranha-supremo-(ua)-1.png" alt="Retângulo 1" class="img-fluid" data-toggle="modal" data-target="#ClassicBenFourArmsModal">
+            </div>
+
+            <div id="alien-card-description">
+                <a href="/" target="_blank">
+                    <p>Aquatico</p>
+                </a>
+            </div>
+                
+        </li>
+
+        <li id="alien-card" class="ripJaws">
+            
+            <div id="alien-card-img">
+                <img src="./macaco-aranha-supremo-(ua)-1.png" alt="Retângulo 1" class="img-fluid" data-toggle="modal" data-target="#ClassicBenFourArmsModal">
+            </div>
+
+            <div id="alien-card-description">
+                <a href="/" target="_blank">
+                    <p>Aquatico</p>
+                </a>
+            </div>
+                
+        </li>
+
+        <li id="alien-card" class="ripJaws">
+            
+            <div id="alien-card-img">
+                <img src="./macaco-aranha-supremo-(ua)-1.png" alt="Retângulo 1" class="img-fluid" data-toggle="modal" data-target="#ClassicBenFourArmsModal">
+            </div>
+
+            <div id="alien-card-description">
+                <a href="/" target="_blank">
+                    <p>Aquatico</p>
+                </a>
+            </div>
+                
+        </li>
+
+        <li id="alien-card" class="ripJaws">
+            
+            <div id="alien-card-img">
+                <img src="./macaco-aranha-supremo-(ua)-1.png" alt="Retângulo 1" class="img-fluid" data-toggle="modal" data-target="#ClassicBenFourArmsModal">
+            </div>
+
+            <div id="alien-card-description">
+                <a href="/" target="_blank">
+                    <p>Aquatico</p>
+                </a>
+            </div>
+                
+        </li>
+
+    </ul>
+    
+    
+    `
+
+    return ultimateAlienTab
+}
+
+function omniverseTab() {
+
+    let omniverseTab = document.getElementById('nav-tab-content').innerHTML = `
+    <div id="sinopse">
+
+        <h1>SINOPSE</h1>
+        <p> 
+            Ben recebe um novo Omnitrix, contendo as transformações da série <a href="#classic-aliens-ov">Clássica</a>,<a href="#alienForce-aliens-ov"> Força Alienígena</a>,<a href="#ultimate-aliens-ov"> Supremacia Alienígena</a>, além de novos aliens incríveis. Esta versão é a original, ele so havia recebido protótipos ao longo de sua jornada, e essa versão começou a ser produzida logo quando ganhou seu primeiro Omnitrix. No entanto, este Omnitrix ainda está em fase de testes e nem sempre fornece ao Ben a transformação no alien que ele pediu, transformando-o em outra coisa, o que cria situações engraçadas para o nosso herói.
+        </p>
+
+    </div>
+
+    <div id="indice-list">
+
+        <p>Índice</p>
+
+        <li><a href="#classic-aliens-ov">Aliens do Clássico</a></li>
+        <li><a href="#alienForce-aliens-ov">Aliens do Força Alienígena</a></li>
+        <li><a href="#ultimate-aliens-ov">Aliens do Supremacia Alienígena</a></li>
+
+
+    </div>
+
+    <!--PRIME BEN'S ALIEN CARDS-->
+    <ul id="aliens-cards">
+
+        <li id="alien-card" class="ripJaws">
+            
+            <div id="alien-card-img">
+                <img src="./astrodactilo-(omniverse)-2.png" alt="Retângulo 1" class="img-fluid" data-toggle="modal" data-target="#ClassicBenFourArmsModal">
+            </div>
+
+            <div id="alien-card-description">
+                <a href="/" target="_blank">
+                    <p>Aquatico</p>
+                </a>
+            </div>
+                
+        </li>
+
+        <li id="alien-card" class="ripJaws">
+            
+            <div id="alien-card-img">
+                <img src="./astrodactilo-(omniverse)-2.png" alt="Retângulo 1" class="img-fluid" data-toggle="modal" data-target="#ClassicBenFourArmsModal">
+            </div>
+
+            <div id="alien-card-description">
+                <a href="/" target="_blank">
+                    <p>Aquatico</p>
+                </a>
+            </div>
+                
+        </li>
+
+        <li id="alien-card" class="ripJaws">
+            
+            <div id="alien-card-img">
+                <img src="./astrodactilo-(omniverse)-2.png" alt="Retângulo 1" class="img-fluid" data-toggle="modal" data-target="#ClassicBenFourArmsModal">
+            </div>
+
+            <div id="alien-card-description">
+                <a href="/" target="_blank">
+                    <p>Aquatico</p>
+                </a>
+            </div>
+                
+        </li>
+
+        <li id="alien-card" class="ripJaws">
+            
+            <div id="alien-card-img">
+                <img src="./astrodactilo-(omniverse)-2.png" alt="Retângulo 1" class="img-fluid" data-toggle="modal" data-target="#ClassicBenFourArmsModal">
+            </div>
+
+            <div id="alien-card-description">
+                <a href="/" target="_blank">
+                    <p>Aquatico</p>
+                </a>
+            </div>
+                
+        </li>
+
+    </ul>
+    
+    
+    `
+
+    return omniverseTab
+}
+
+function rebootTab() {
+
+    let rebootTab = document.getElementById('nav-tab-content').innerHTML = `
+    <div id="sinopse">
+
+        <h1>SINOPSE</h1>
+        <p> 
+            Com base na franquia de grande sucesso sobre o herói infantil Ben Tennyson, Ben 10 apresenta um Ben re-imaginado, sua prima Gwen e o vovô Max, enquanto viajam pelo país durante as férias de verão. Quando Ben encontra o Omnitrix, um relógio misterioso que o transforma em 10 alienígenas diferentes, um mundo de superpoderes extraterrestres se abre para ele.
+        </p>
+
+    </div>
+
+    <div id="indice-list">
+
+        <p>Índice</p>
+
+        <li><a href="#omnitunado">Omnitunado</a></li>
+        <li><a href="#omnikix">Omnikix</a></li>
+        <li><a href="#omninauta">Omninauta</a></li>
+
+
+    </div>
+
+    <!--PRIME BEN'S ALIEN CARDS-->
+    <ul id="aliens-cards">
+
+        <li id="alien-card" class="ripJaws">
+            
+            <div id="alien-card-img">
+                <img src="./chama-omnitunado-(reboot)-1.png" alt="Retângulo 1" class="img-fluid" data-toggle="modal" data-target="#ClassicBenFourArmsModal">
+            </div>
+
+            <div id="alien-card-description">
+                <a href="/" target="_blank">
+                    <p>Aquatico</p>
+                </a>
+            </div>
+                
+        </li>
+
+        <li id="alien-card" class="ripJaws">
+            
+            <div id="alien-card-img">
+                <img src="./chama-omnitunado-(reboot)-1.png" alt="Retângulo 1" class="img-fluid" data-toggle="modal" data-target="#ClassicBenFourArmsModal">
+            </div>
+
+            <div id="alien-card-description">
+                <a href="/" target="_blank">
+                    <p>Aquatico</p>
+                </a>
+            </div>
+                
+        </li>
+
+        <li id="alien-card" class="ripJaws">
+            
+            <div id="alien-card-img">
+                <img src="./chama-omnitunado-(reboot)-1.png" alt="Retângulo 1" class="img-fluid" data-toggle="modal" data-target="#ClassicBenFourArmsModal">
+            </div>
+
+            <div id="alien-card-description">
+                <a href="/" target="_blank">
+                    <p>Aquatico</p>
+                </a>
+            </div>
+                
+        </li>
+
+        <li id="alien-card" class="ripJaws">
+            
+            <div id="alien-card-img">
+                <img src="./chama-omnitunado-(reboot)-1.png" alt="Retângulo 1" class="img-fluid" data-toggle="modal" data-target="#ClassicBenFourArmsModal">
+            </div>
+
+            <div id="alien-card-description">
+                <a href="/" target="_blank">
+                    <p>Aquatico</p>
+                </a>
+            </div>
+                
+        </li>
+
+    </ul>
+    
+    
+    `
+
+    return rebootTab
+}
