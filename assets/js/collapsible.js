500a914b12af46d1aa96298a8e1ad1b59e23a782
CaioLima96 <62268811+CaioLima96@users.noreply.github.com>
2021-11-08 17:37:42 -0300

    adicionado a page de fanfics

--- assets/js/collapsible.js
+++ assets/js/collapsible.js
@@ -1,14 +1,15 @@
-var coll = document.getElementsByClassName("collapsible");
-        var i;
+let coll = document.getElementsByClassName("rulesCollapsible");
 
-        for (i = 0; i < coll.length; i++) {
-        coll[i].addEventListener("click", function() {
-            this.classList.toggle("active");
-            var content = this.nextElementSibling;
-            if (content.style.maxHeight){
-            content.style.maxHeight = null;
-            } else {
-            content.style.maxHeight = content.scrollHeight + "px";
-            }
-        });
+let i;
+
+for (i = 0; i < coll.length; i++) {
+    coll[i].addEventListener("click", function() {
+        this.classList.toggle("rulesActive");
+        let content = this.nextElementSibling;
+        if (content.style.maxHeight){
+        content.style.maxHeight = null;
+        } else {
+        content.style.maxHeight = content.scrollHeight + "px";
+        }
+    });
 }
