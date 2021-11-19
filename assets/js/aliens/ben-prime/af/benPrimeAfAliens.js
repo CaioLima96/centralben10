0cf531fd76320c108fb52932233094a8718cda45
CaioLima96 <62268811+CaioLima96@users.noreply.github.com>
2021-11-19 02:46:23 -0300

    testando o js dos aliens

---
+++ assets/js/aliens/ben-prime/af/benPrimeAfAliens.js
@@ -0,0 +1,9 @@
+const arrBenPrimeAfAliens = [
+    {nomePtbr: 'Alien X', nomeIngles: 'Alien X', img: ['./assets/img/aliens/ben-prime/af/alien-x-(af)-1.png', './assets/img/aliens/ben-prime/af/alien-x-(af)-2.png']},
+
+    {nomePtbr: 'Arraia Jato', nomeIngles: 'Jetray', img: ['./assets/img/aliens/ben-prime/af/arraia-jato-(af)-1.png', './assets/img/aliens/ben-prime/af/arraia-jato-(af)-2.png', './assets/img/aliens/ben-prime/af/arraia-jato-(af)-3.png', './assets/img/aliens/ben-prime/af/arraia-jato-(af)-4.png']},
+
+    {nomePtbr: 'Artrópode', nomeIngles: 'Brainstorm', img: ['./assets/img/aliens/ben-prime/af/artropode-(af)-1.png', './assets/img/aliens/ben-prime/af/artropode-(af)-2.png', './assets/img/aliens/ben-prime/af/artropode-(af)-3.png', './assets/img/aliens/ben-prime/af/artropode-(af)-4.png']},
+
+    {nomePtbr: 'Bebê Necrofriggiano', nomeIngles: "Big Chill's Offspring", img: ['./assets/img/aliens/ben-prime/af/bebe-necrofrigiano-(af)-1.png']}
+]
