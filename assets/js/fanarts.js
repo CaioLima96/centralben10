61d9c237dc2712122a8301a139c6b1b8dc6ea0e7
CaioLima96 <62268811+CaioLima96@users.noreply.github.com>
2021-11-20 20:38:15 -0300

    adicionado as fanarts via dom

---
+++ assets/js/fanarts.js
@@ -0,0 +1,191 @@
+function fanArtTab1() {
+
+    let teste1 = document.getElementById("nav-tab-content")
+    
+    let fanArtsTab1 = teste1.innerHTML = `
+        <!--FANARTS CARDS-->
+        <ul class="grid-cards">
+
+            <li class="card">
+                            
+                <div class="card-img">
+                    <img src="../img/fanarts/styven-porto-alves-1.jpg" alt="Lucas Felix" >
+                </div>
+
+                <div class="author-info">
+
+                    <div class="text">
+
+                        <div>
+                            <a href="https://www.instagram.com/_styven_artes/"><img src="../img/icons/insta-icon.png" target="_blank" alt="instagram icon"></a>
+
+                            <a href="https://www.facebook.com/styven.alvesporto.9"><img src="../img/icons/facebook-icon.png" target="_blank"  alt="Facebook icon"></a>
+                        </div>
+
+                        <a href="https://www.instagram.com/luc_fel/" target="_blank">
+                            <p>_styven_artes</p>
+                        </a>
+
+                    </div>
+
+                </div>
+            
+            </li>
+
+        <li class="card">
+                
+            <div class="card-img">
+                <img src="../img/fanarts/lucas-felix-2.jpg" alt="Lucas Felix" >
+            </div>
+
+            <div class="author-info">
+
+                <div class="text">
+
+                    <div>
+                        <a href="https://www.instagram.com/luc_fel/"><img src="../img/icons/insta-icon.png" target="_blank" alt="instagram icon"></a>
+                    </div>
+
+                    <a href="https://www.instagram.com/luc_fel/" target="_blank">
+                        <p>luc_fel</p>
+                    </a>
+
+                </div>
+
+            </div>
+            
+        </li>
+
+        <li class="card">
+                
+            <div class="card-img">
+                <img src="../img/fanarts/lucas-felix-1.jpg" alt="Lucas Felix" >
+            </div>
+
+            <div class="author-info">
+
+                <div class="text">
+
+                    <div>
+                        <a href="https://www.instagram.com/luc_fel/"><img src="../img/icons/insta-icon.png" target="_blank" alt="instagram icon"></a>
+                    </div>
+
+                    <a href="https://www.instagram.com/luc_fel/" target="_blank">
+                        <p>luc_fel</p>
+                    </a>
+
+                </div>
+
+            </div>
+            
+        </li>
+
+        <li class="card">
+                
+            <div class="card-img">
+                <img src="../img/fanarts/caiolimaart-2.jpg" alt="Caio Vieira Lima" >
+            </div>
+
+            <div class="author-info">
+
+                <div class="text">
+
+                    <div>
+                        <a href="https://www.deviantart.com/caiolimaart"><img src="../img/icons/deviantart-icon.png" alt="DeviantArt Icon"></a>
+
+                        <a href="https://www.instagram.com/caiolimaart/"><img src="../img/icons/insta-icon.png" target="_blank" alt="instagram icon"></a>
+
+                        <a href="https://www.facebook.com/caiolimaart"><img src="../img/icons/facebook-icon.png" target="_blank"  alt="Facebook icon"></a>
+                    </div>
+
+                    <a href="https://www.instagram.com/caiolimaart/" target="_blank">
+                        <p>Caio Vieira Lima</p>
+                    </a>
+
+                </div>
+
+            </div>
+            
+        </li>
+            
+            <li class="card">
+                                        
+                <div class="card-img">
+                    <img src="../img/fanarts/leoZin-1.jpg" alt="leoZin" >
+                </div>
+
+                <div class="author-info">
+
+                    <div class="text">
+
+                        <div>
+                            <a href="https://www.facebook.com/profile.php?id=100029848282342"><img src="../img/icons/facebook-icon.png" target="_blank"  alt="Facebook icon"></a>
+                        </div>
+
+                        <a href="https://www.instagram.com/luis.edu05/" target="_blank">
+                            <p>leoZin</p>
+                        </a>
+
+                    </div>
+
+                </div>
+
+            </li>
+
+            <li class="card">
+                                
+                <div class="card-img">
+                    <img src="../img/fanarts/luis.edu05-1.jpg" alt="luis.edu05" >
+                </div>
+
+                <div class="author-info">
+
+                    <div class="text">
+
+                        <div>
+                            <a href="https://www.instagram.com/luis.edu05/"><img src="../img/icons/insta-icon.png" target="_blank" alt="instagram icon"></a>
+                        </div>
+
+                        <a href="https://www.instagram.com/luis.edu05/" target="_blank">
+                            <p>Luis Eduardo</p>
+                        </a>
+
+                    </div>
+
+                </div>
+        
+            </li>
+
+            <li class="card">
+                            
+                <div class="card-img">
+                    <img src="../img/fanarts/caiolimaart-1.jpg" alt="Caio Vieira Lima" >
+                </div>
+
+                <div class="author-info">
+
+                    <div class="text">
+
+                        <div>
+                            <a href="https://www.deviantart.com/caiolimaart"><img src="../img/icons/deviantart-icon.png" alt="DeviantArt Icon"></a>
+
+                            <a href="https://www.instagram.com/caiolimaart/"><img src="../img/icons/insta-icon.png" target="_blank" alt="instagram icon"></a>
+
+                            <a href="https://www.facebook.com/caiolimaart"><img src="../img/icons/facebook-icon.png" target="_blank"  alt="Facebook icon"></a>
+                        </div>
+
+                        <a href="https://www.instagram.com/caiolimaart/" target="_blank">
+                            <p>Caio Vieira Lima</p>
+                        </a>
+
+                    </div>
+
+                </div>
+            
+            </li>
+
+        </ul>
+    `
+    console.log('abc')
+    return fanArtsTab1
+}
