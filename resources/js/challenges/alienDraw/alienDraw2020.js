e06009a6e7b1e514d22ad3b5c1b6c37d344641ef
CaioLima96 <62268811+CaioLima96@users.noreply.github.com>
2021-12-12 19:55:53 -0300

    FEAT: adicionado a aba de desafio de 2020 do AD

--- resources/js/challenges/alienDraw/alienDraw2020.js
+++ resources/js/challenges/alienDraw/alienDraw2020.js
@@ -17,6 +17,7 @@ function alienDraw2020Tab(){
         // hello world
     // `
 
+    console.log('teste 2020')
     let tab2020 = document.getElementById("challengeTab").innerHTML = `
         <div id="ad2020tab">
             <!--CHALLENGE-MENU-->
@@ -24,13 +25,105 @@ function alienDraw2020Tab(){
 
                 <nav>
                     <ul>
-                        <li>JAN</li>
+                        <li style="border: 1px solid white;">SET</li>
                     </ul>
                 </nav>
 
             </div>
 
-            hello world
+            <div id="nav-tab-content" class="nav-tab-content-2021">
+
+                <!--NAV TAB CONTENT-->
+                <div class="challenge-theme">
+                    <p>
+                        Tema: <a href="https://www.facebook.com/groups/Ben10cidadedebaixo/permalink/680513452559559/" target="_blank">Aliens Supremos</a><br> Data: 11/9/2020
+                    </p>
+                </div>
+
+            
+
+                <ul class="grid-cards">
+                                
+                    <li class="card firstPlace">
+                        
+                        <div class="card-img">
+                            <img src="../../../assets/img/challenges/aliendraw/2020/tenth-edition-aliens-supremos/first-place-felipe-sabino.jpg" alt="fesenha" >
+                        </div>
+
+                        <div class="author-info">
+
+                            <div class="text">
+
+                                <div>
+                                    <a href="https://www.instagram.com/fesenha/" target="_blank"><img src="../../../assets/img/icons/insta-icon.png" alt="instagram icon"></a>
+
+                                    <a href="https://www.facebook.com/Fesenha-Arts-106001021180786" target="_blank"><img src="../../../assets/img/icons/facebook-icon.png"  alt="Facebook icon"></a>
+                                </div>
+
+                                <a href="https://www.instagram.com/fesenha/" target="_blank">
+                                    <p>Fesenha</p>
+                                </a>
+
+                            </div>
+
+                        </div>
+                        
+                    </li>
+
+                    <li class="card secondPlace">
+                        
+                        <div class="card-img">
+                            <img src="../../../assets/img/challenges/aliendraw/2020/tenth-edition-aliens-supremos/second-place-joao-lucas-oliveira.jpg" alt="jeyel_art" >
+                        </div>
+
+                        <div class="author-info">
+
+                        <div class="text">
+
+                            <div>
+                                <a href="https://www.instagram.com/jeyel_art/" target="_blank"><img src="../../../assets/img/icons/insta-icon.png" alt="instagram icon"></a>
+                            </div>
+
+                            <a href="https://www.instagram.com/jeyel_art/" target="_blank">
+                                <p>jeyel_art</p>
+                            </a>
+
+                        </div>
+
+                    </div>
+                        
+                    </li>
+                    
+                    <li class="card thirdPlace">
+                        
+                        <div class="card-img">
+                            <img src="../../../assets/img/challenges/aliendraw/2020/tenth-edition-aliens-supremos/third-place-caiolimaart.jpg" alt="caiolimaart" >
+                        </div>
+
+                        <div class="author-info">
+
+                            <div class="text">
+
+                                <div>
+                                    <a href="https://www.instagram.com/caiolimaart/" target="_blank"><img src="../../../assets/img/icons/insta-icon.png" alt="instagram icon"></a>
+
+                                    <a href="https://www.facebook.com/caiolimaart" target="_blank"><img src="../../../assets/img/icons/facebook-icon.png" alt="Facebook icon"></a>
+                                </div>
+
+                                <a href="https://www.instagram.com/caiolimaart/" target="_blank">
+                                    <p>CaioLimaArt</p>
+                                </a>
+
+                            </div>
+
+                        </div>
+                        
+                    </li>
+
+                </ul>
+            </div>
+            
+
         </div>
     `
 
