eabb4f98af1235ce2f837cd683ebda8fd615f6e1
CaioLima96 <62268811+CaioLima96@users.noreply.github.com>
2021-12-12 18:15:25 -0300

    FEAT: adicionado os participantes do segundo AD fev 2021

--- resources/js/challenges/alienDraw/alienDraw2021.js
+++ resources/js/challenges/alienDraw/alienDraw2021.js
@@ -133,17 +133,10 @@ function alienDraw2021JanTab() {
 
 function alienDraw2021FevTab(){
 
-    let ad2021Fevtab = document.getElementById("nav-tab-content").innerHTML = `
-
-        <!--NAV TAB CONTENT-->
-        <div class="challenge-theme">
-            <p>
-                Tema: <a href="https://www.facebook.com/groups/Ben10cidadedebaixo/permalink/787418341869069/" target="_blank">Folclore</a><br> Data: 10/02/21 - 20/02/21
-            </p>
-        </div>
-
+    let desafio1Fev2021 = `
         <!--DESAFIO 1-->
         <ul class="grid-cards">
+
             <li class="card firstPlace">
                                     
                 <div class="card-img">
@@ -167,7 +160,157 @@ function alienDraw2021FevTab(){
                 </div>
             
             </li>
+
+            <li class="card secondPlace">
+                                    
+                <div class="card-img">
+                    <img src="../../../assets/img/challenges/aliendraw/2021/02ª-edition-folclore/second-place-luigy_martins.jpg" alt="luigy_martins" >
+                </div>
+
+                <div class="author-info">
+
+                    <div class="text">
+
+                        <div>
+                            <a href="https://www.instagram.com/_luigy._/" target="_blank"><img src="../../../assets/img/icons/insta-icon.png" alt="instagram icon"></a>
+
+                            <a href="https://www.artstation.com/luigy_martins" target="_blank"><img src="../../../assets/img/icons/artstations-icon.png" alt="artstation icon"></a>
+                        </div>
+
+                        <a href="https://www.artstation.com/luigy_martins" target="_blank">
+                            <p>Luigy Martins</p>
+                        </a>
+
+                    </div>
+
+                </div>
+                
+            </li>
+            
+            <li class="card thirdPlace">
+                
+                <div class="card-img">
+                    <img src="../../../assets/img/challenges/aliendraw/2021/02ª-edition-folclore/third-place-joao-lucas-oliveira.jpg" alt="jeyel_art" >
+                </div>
+
+                <div class="author-info">
+
+                    <div class="text">
+
+                        <div>
+                            <a href="https://www.instagram.com/jeyel_art/" target="_blank"><img src="../../../assets/img/icons/insta-icon.png" alt="instagram icon"></a>
+                        </div>
+
+                        <a href="https://www.instagram.com/jeyel_art/" target="_blank">
+                            <p>jeyel_art</p>
+                        </a>
+
+                    </div>
+
+                </div>
+                
+            </li>
+
         </ul>
+    `
+
+    let desafio2Fev2021 = `
+        <ul class="grid-cards">
+                                
+            <li class="card firstPlace">
+                
+                <div class="card-img">
+                    <img src="../../../assets/img/challenges/aliendraw/2021/03ª-edition-mitologias/first-place-joao-lucas-oliveira.jpg" alt="jeyel_art" >
+                </div>
+
+                <div class="author-info">
+
+                    <div class="text">
+
+                        <div>
+                            <a href="https://www.instagram.com/jeyel_art/" target="_blank"><img src="../../../assets/img/icons/insta-icon.png" alt="instagram icon"></a>
+                        </div>
+
+                        <a href="https://www.instagram.com/jeyel_art/" target="_blank">
+                            <p>jeyel_art</p>
+                        </a>
+
+                    </div>
+
+                </div>
+                
+            </li>
+
+            <li class="card secondPlace">
+                
+                <div class="card-img">
+                    <img src="../../../assets/img/challenges/aliendraw/2021/03ª-edition-mitologias/second-place-kailane-nunes.jpg" alt="kaeni_art" >
+                </div>
+
+                <div class="author-info">
+
+                    <div class="text">
+
+                        <div>
+                            <a href="https://www.instagram.com/kaeni_art/" target="_blank"><img src="../../../assets/img/icons/insta-icon.png" alt="instagram icon"></a>
+                        </div>
+
+                        <a href="https://www.instagram.com/kaeni_art/" target="_blank">
+                            <p>kaeni_art</p>
+                        </a>
+
+                    </div>
+
+                </div>
+                
+            </li>
+            
+            <li class="card thirdPlace">
+                
+                <div class="card-img">
+                    <img src="../../../assets/img/challenges/aliendraw/2021/03ª-edition-mitologias/third-place-taka-altair.jpg" alt="Taka Altair" >
+                </div>
+
+                <div class="author-info">
+
+                    <div class="text">
+
+                        <div>
+                            <a href="https://www.facebook.com/photo?fbid=1648857678640295&set=pcb.794689057808664" target="_blank"><img src="../../../assets/img/icons/insta-icon.png" alt="instagram icon"></a>
+                        </div>
+
+                        <a href="https://www.facebook.com/photo?fbid=1648857678640295&set=pcb.794689057808664" target="_blank">
+                            <p>Taka Altair</p>
+                        </a>
+
+                    </div>
+
+                </div>
+                
+            </li>
+
+        </ul>
+    `
+
+    let ad2021Fevtab = document.getElementById("nav-tab-content").innerHTML = `
+
+        <!--NAV TAB CONTENT-->
+        <div class="challenge-theme">
+            <p>
+                Tema: <a href="https://www.facebook.com/groups/Ben10cidadedebaixo/permalink/787418341869069/" target="_blank">Folclore</a><br> Data: 10/02/21 - 20/02/21
+            </p>
+        </div>
+
+        ${desafio1Fev2021}
+
+        <div class="challenge-theme">
+            <p>
+                Tema: <a href="https://www.facebook.com/groups/Ben10cidadedebaixo/permalink/794689057808664/" target="_blank">Mitologias</a><br> Data: 24/02/21 - 06/03/21
+            </p>
+        </div>
+
+        ${desafio2Fev2021}
+        
     `
     console.log('fevereiro 2021')
     return ad2021Fevtab
