716c7d06d987a737f99ec7aefeea31bec9309887
CaioLima96 <62268811+CaioLima96@users.noreply.github.com>
2022-04-12 19:07:15 -0300

    REFACTOR: global.css criado, css do header/footer/navbar tudo no mesmo arquivo

--- resources/js/components/navbar.js
+++ resources/js/components/navbar.js
@@ -1,13 +1,4 @@
-// let a = document.getElementById("teste1")
-// let b = document.getElementById("navBarLista")
-// a.addEventListener('click', function() {
-//     b.classList.toggle('hideShowBtn')
-//   })
-
-
 function myNavBar() {
-  //let c = document.getElementById('data-buton')
-  let lista = document.getElementById("navBarLista")
-  lista.classList.toggle('hideShowBtn')
-  console.log('teste')
+	let lista = document.getElementById("navBarLista")
+	lista.classList.toggle('hideShowBtn')
 }
