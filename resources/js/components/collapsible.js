36e4179ecef0f22588170037f7a4a0351a7407ca
CaioLima96 <62268811+CaioLima96@users.noreply.github.com>
2022-04-12 19:25:54 -0300

    REFACTOR: refatorado css do rules

--- resources/js/components/collapsible.js
+++ resources/js/components/collapsible.js
@@ -3,13 +3,21 @@ let coll = document.getElementsByClassName("rulesCollapsible");
 let i;
 
 for (i = 0; i < coll.length; i++) {
+
     coll[i].addEventListener("click", function() {
+
         this.classList.toggle("rulesActive");
+
         let content = this.nextElementSibling;
+        
         if (content.style.maxHeight){
-        content.style.maxHeight = null;
+
+            content.style.maxHeight = null;
+
         } else {
-        content.style.maxHeight = content.scrollHeight + "px";
+            
+            content.style.maxHeight = content.scrollHeight + "px";
         }
+
     });
 }
